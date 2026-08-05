5a2d79b1d48f7084ccb080130af1774ee031bcd7
haeseok <haeseok_@naver.com>
2026-08-05 16:41:28 +0900

    style: 로그인 페이지 스타일 수정

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,14 +1,15 @@
 <template>
     <button
         :class="[
-            'rounded-lg transition font-medium cursor-pointer',
+            'transition font-medium cursor-pointer',
             themeClass,
             sizeClass,
+            roundedClass,
             {
                 'opacity-50 cursor-not-allowed': disabled || loading
             }
         ]"
-        @click="handleClick"
+        @click="clickHandler"
     >
         <slot />
     </button>
@@ -18,62 +19,81 @@
     import { computed } from 'vue'
     import { useRouter } from 'vue-router'
 
-    const router = useRouter()
-    const emit = defineEmits(['click'])
+    // 버튼 설정
     const props = defineProps({
-        theme: {
-            type: String,
-            defaults: 'primary'
-        },
-        size: {
-            type: String,
-            defaults: 'm'
-        },
-        disabled: {
-            type: Boolean,
-            defaults: false
-        },
-        loading: {
-            type: Boolean,
-            defaults: false
-        },
-        to: {
-            type: String,
-            defaults: null
-        }
+        theme: { type: String, defaults: 'primary'},
+        size: { type: String, default: 'md' },
+        rounded: { type: Boolean, default: true },
+        disabled: { type: Boolean, default: false },
+        loading: { type: Boolean, default: false },
+        to: { type: String, default: null },
+        width: { type: String, default: '' },
+        height: { type: String, default: '' }
     })
 
-    // 버튼의 색상 테마를 지정
+    // 조건별 클래스 설정
     const themeClass = computed(() => {
         switch(props.theme) {
-            case 'transparent':
-                return 'bg-transparent bd-none'
-            case 'line':
-                return 'bg-white bd-black'
-                break;
-            default: 
-                return 'bg-sky-500 text-white hover:bg-sky-600'
+            case 'lightgray' :
+                return 'bg-gray-200 text-[#1A3553] hover:bg-gray-300'
+            default :
+                return 'bg-[#1A3553] text-gray-100 hover:bg-[#112338]'
         }
     })
 
-    // 버튼의 사이즈를 지정
     const sizeClass = computed(() => {
+        let baseSizeClass = '';
+
         switch(props.size) {
-            default:
-                return 'px-3 py-1'
+            case 'xs' :
+                baseSizeClass = 'px-2 py-1 text-xs'
+                break
+            case 'sm' : 
+                baseSizeClass = 'px-2 py-1.5 text-sm'
+                break
+            case 'lg' :
+                baseSizeClass = 'px-4 py-1.5 text-lg'
+                break
+            case 'xl' :
+                baseSizeClass = 'px-4 py-2 text-xl'
+                break
+            case 'full' :
+                baseSizeClass = props.height
+                    ? (props.width ? '' : 'w-full')
+                    : (props.width ? 'h-full py-3' : 'w-full h-full')
+                break
+            default : 
+                baseSizeClass = 'px-3 py-1.5 text-base'
+        }
+
+        return `${baseSizeClass} ${props.width} ${props.height}`.trim()
+    })
+
+    const roundedClass = computed(() => {
+        switch(props.rounded) {
+            case true :
+                return 'rounded-lg'
+            default: 
+                return ''
         }
     })
 
     // 버튼 클릭 핸들러
-    const handleClick = event => {
-        if( props.disabled || props.loading ) return
-
-        // to 파라미터가 있을경우 라우터 이동 처리
-        if( props.to ) {
-            router.push(props.to)
+    const router = useRouter()
+    const emit = defineEmits(['click'])
+    const clickHandler = event => {
+        // 로딩중이거나 비활성화 상태라면 예외처리
+        if( props.disabled || props.loading ) {
+            return
         }
 
-        // to 파라미터가 없을 경우 기존 이벤트 발생
-        emit('click', event);
+        // to 파라미터가 있을경우 라우터 이동처리
+        if( props.to ) {
+            router.push(props.to)
+            return
+        }
+
+        // 기본 이벤트 처리
+        emit('click', event)
     }
 </script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,24 +2,55 @@
     <main>
         <div class="login__responsive h-screen flex items-center">
             <div class="login__visual w-[calc(100%-800px)] flex justify-center">
-                <BaseButton type="button" to="/" theme="transparent">
-                    <img src="@/assets/image/logo-vertical.svg" width="200" alt="">
-                </BaseButton>
+                <div class="visual__content text-center">
+                    <RouterLink to="/">
+                        <img src="@/assets/image/logo.svg" width="200" alt="">
+                    </RouterLink>
+                    <p class="mt-5 font-bold text-2xl">환영합니다!</p>
+                </div>
             </div>
-            <div class="login__content w-[800px] h-full bg-white flex flex-col justify-center">
-                <h3>로그인</h3>
-                <p>이메일로 로그인</p>
-                <form @submit.prevent="loginCheck">
-                    <input type="text" v-model="form.email" placeholder="이메일을 입력하세요.">
-                    <button type="submit">로그인</button>
-                </form>
-                <div class="login__oauth">
-                    <p>소셜 계정으로 로그인</p>
-                    <ul>
-                        <li>깃허브</li>
-                        <li>구글</li>
-                        <li>디스코드</li>
-                    </ul>
+            <div class="login__content w-[800px] h-full bg-white flex items-center px-10">
+                <div class="w-[400px]">
+                    <h3 class="text-2xl font-bold mb-10">repoHub</h3>
+                    <form @submit.prevent="loginCheck">
+                        <BaseInput
+                            v-model="form.email.value"
+                            label="이메일로 로그인"
+                            placeholder="이메일을 입력하세요."
+                            :error="form.email.error"
+                        >
+                            <template #append>
+                                <BaseButton
+                                    type="submit"
+                                    width="w-[100px]"
+                                    height="h-[40px]"
+                                    rounded="false"
+                                >
+                                    로그인
+                                </BaseButton>
+                            </template>
+                        </BaseInput>
+                    </form>
+                    <div class="login__oauth mt-5">
+                        <p class="text-gray-400">소셜 계정으로 로그인</p>
+                        <ul class="flex items-center gap-10 text-white mt-4">
+                            <li class="w-[50px] h-[50px] bg-[#1A3553] rounded-full">
+                                <a href="" class="flex w-full h-full justify-center items-center">
+                                    <SimpleIcon :size="24" :icon="siGithub" />
+                                </a>
+                            </li>
+                            <li class="w-[50px] h-[50px] bg-[#1A3553] rounded-full">
+                                <a href="" class="flex w-full h-full justify-center items-center">
+                                    <SimpleIcon :size="24" :icon="siGoogle" />
+                                </a>
+                            </li>
+                            <li class="w-[50px] h-[50px] bg-[#1A3553] rounded-full">
+                                <a href="" class="flex w-full h-full justify-center items-center">
+                                    <SimpleIcon :size="24" :icon="siDiscord" />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
@@ -28,17 +59,38 @@
 
 <script setup>
     import { reactive } from 'vue'
+    import { useAuthStore } from '@/stores/auth'
+    import { useRouter } from 'vue-router'
+    import { isValidEmail } from '@/utils/validation';
+    import { siGoogle, siGithub, siDiscord } from 'simple-icons/icons'
+    import SimpleIcon from '@/components/SimpleIcon.vue';
     import BaseButton from '@/components/BaseButton.vue';
+    import BaseInput from '@/components/BaseInput.vue';
 
-    const form = reactive({ email: '' })
+    console.log(siGithub)
+
+    const router = useRouter()
+    const auth = useAuthStore();
+
+    const form = reactive({
+        email: { value: '', error: '' }
+    })
+
     const loginCheck = () => {
-        
-        if( !form.email ) {
-            alert('이메일을 입력해주세요.')
+
+        // 유효성 체크
+        if( !form.email.value ) {
+            form.email.error = '이메일을 입력해주세요.'
+            return
+        }
+        if( !isValidEmail(form.email.value) ) {
+            form.email.error = '이메일 형식을 확인해주세요.'
             return
         }
 
-        alert('로그인 성공')
+        // 로그인 성공 처리
+        auth.login('test', 'test');
+        router.push('/')
     }
 </script>
 
